367543f6b206c0f13f412c5fe37e03777480e0d0
Elijah Rieger <elijah.rieger@flinders.edu.au>
2026-05-28 21:13:23 +0930

    Tests 5.09 and 5.10 - export different types

--- Tests/TimetableModeTest.java
+++ Tests/TimetableModeTest.java
@@ -264,7 +264,9 @@ class TimetableModeTest {
     @Tag("Elijah")
     @Tag("Core")
     @Test
-    void exportPossibleFile() throws Exception {}
+    void exportPossibleFile() throws Exception {
+        // Potential logical duplicate of 5.05 please see gantt chart comments.
+    }
 
     @DisplayName("5.08 Exporting in TSV")
     @Tag("Numa")
@@ -276,13 +278,90 @@ class TimetableModeTest {
     @Tag("Elijah")
     @Tag("Additional")
     @Test
-    void exportJSON() throws Exception {}
+    void exportJSON() throws Exception  {
+        String userHome = System.getProperty("user.home");
+        Path outPath = Path.of(userHome + "/Exported Timetables/testTimetable1.json");
 
+        String input =  "4\n" + // Enter timetable mode
+                "1\n" + // Create a timetable
+                "testTimetable1\n" + // Name timetable
+                "\n" + // Default semester
+                "COMP1002, COMP1102, COMP1103\n" + // Select topics
+                "\n" + // Default campus
+                "yes\n" + // Allow lecture overlap
+                "\n" + // Default preference order
+                "5\n" + // Export mode
+                "1\n" + // Select fist (and only) timetable
+                "3\n" + // JSON export
+                "\n"+ // Null file path
+                "0\n" + // Exit timetable mode
+                "0\n"; // Exit app
+
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        TimetableApp.main(new String[]{"data-loader/timetable.db"});
+        String output = captureOutputStream.toString();
+
+//        System.err.println("---- CAPTURED OUTPUT ----");
+//        System.err.println(output);
+//        System.err.println("-------------------------");
+
+        assertAll(
+                () -> assertTrue(output.contains("Generated timetable")),
+                () -> assertTrue(output.contains("EXPORT GENERATED TIMETABLE")),
+                () -> assertTrue(output.contains("Output file path")),
+                () -> assertTrue(output.contains("Exported timetable to:")),
+                () -> assertTrue(output.contains("/Exported Timetables/testTimetable1.json")),
+                () -> assertTrue(Files.exists(outPath))
+        );
+
+        Files.deleteIfExists(outPath);
+        Files.deleteIfExists(outPath.getParent());
+    }
+
+    // Logical duplicate of other other export tests which use csv for testing.
     @DisplayName("5.10 Exporting in CSV")
     @Tag("Elijah")
     @Tag("Core")
     @Test
-    void exportCSV() throws Exception {}
+    void exportCSV() throws Exception  {
+        String userHome = System.getProperty("user.home");
+        Path outPath = Path.of(userHome + "/Exported Timetables/testTimetable1.csv");
+
+        String input =  "4\n" + // Enter timetable mode
+                "1\n" + // Create a timetable
+                "testTimetable1\n" + // Name timetable
+                "\n" + // Default semester
+                "COMP1002, COMP1102, COMP1103\n" + // Select topics
+                "\n" + // Default campus
+                "yes\n" + // Allow lecture overlap
+                "\n" + // Default preference order
+                "5\n" + // Export mode
+                "1\n" + // Select fist (and only) timetable
+                "1\n" + // CSV export
+                "\n"+ // Null file path
+                "0\n" + // Exit timetable mode
+                "0\n"; // Exit app
+
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        TimetableApp.main(new String[]{"data-loader/timetable.db"});
+        String output = captureOutputStream.toString();
+
+//        System.err.println("---- CAPTURED OUTPUT ----");
+//        System.err.println(output);
+//        System.err.println("-------------------------");
+
+        assertAll(
+                () -> assertTrue(output.contains("Generated timetable")),
+                () -> assertTrue(output.contains("EXPORT GENERATED TIMETABLE")),
+                () -> assertTrue(output.contains("Output file path")),
+                () -> assertTrue(output.contains("Exported timetable to:")),
+                () -> assertTrue(output.contains("/Exported Timetables/testTimetable1.csv")),
+                () -> assertTrue(Files.exists(outPath))
+        );
+
+        Files.deleteIfExists(outPath);
+        Files.deleteIfExists(outPath.getParent());
+    }
 
     @DisplayName("5.11 Generating Timetable error/help messages")
     @Tag("Aidan")
