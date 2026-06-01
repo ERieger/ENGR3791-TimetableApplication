4d016f27372bc6b76121a5a6ef543f14d26d6577
putl0014 <putl0014@flinders.edu.au>
2026-06-01 10:36:29 +0930

    Added scanner import for lucy's test

--- Tests/ClassesViewTest.java
+++ Tests/ClassesViewTest.java
@@ -9,6 +9,7 @@ import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
